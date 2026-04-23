63575a6a396c95aba0664b27a7731c652586e85e
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-04-23 18:19:06 +0200

    Added a delete exercise button to Plan A. Minor visual improvements.

--- app/src/main/java/porwol/krzysztof/myapplication/InterfejsUżytkownika/EkranPlanA.kt
+++ app/src/main/java/porwol/krzysztof/myapplication/InterfejsUżytkownika/EkranPlanA.kt
@@ -1,6 +1,7 @@
 package porwol.krzysztof.myapplication.InterfejsUżytkownika
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +22,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -29,69 +32,84 @@ import porwol.krzysztof.myapplication.ReprezentacjaDanych.TymczasowyZestawPlanó
 @Composable
 fun EkranPlanA(navController: NavController) {
 
-    val listaĆwiczeń = TymczasowyZestawPlanówĆwiczeń.planA
+    val listaĆwiczeń = remember { TymczasowyZestawPlanówĆwiczeń.planA }
     val stanScrolla = rememberScrollState()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .padding(20.dp)
-            .verticalScroll(stanScrolla)
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+                .padding(20.dp)
+                .verticalScroll(stanScrolla)
+        ) {
 
-        Spacer(modifier = Modifier.height(70.dp))
+            Spacer(modifier = Modifier.height(70.dp))
 
-        Text(
-            "Plan A - Ćwiczenia",
-            color = MaterialTheme.colorScheme.onSurface
-        )
+            Text(
+                "Plan A - Ćwiczenia",
+                color = MaterialTheme.colorScheme.onSurface
+            )
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        if (listaĆwiczeń.isEmpty()) {
-            Text("Brak ćwiczeń w planie.")
-        } else {
-            listaĆwiczeń.forEach { pojedyńczeĆwiczenie ->
+            if (listaĆwiczeń.isEmpty()) {
                 Text(
-                    "${pojedyńczeĆwiczenie.nazwa} " +
-                            "- ilość serii: ${pojedyńczeĆwiczenie.serie} ",
+                    "Brak ćwiczeń w planie.",
                     color = MaterialTheme.colorScheme.onSurface
                 )
-
-                // Powtórz tyle razy, ile jest serii -
-                // tutaj repeat przekazuje iteracje ilosci serii (indeks) czyli
-                // tak naprawdę 0, 1 i 2 (w przypadku 3 serii)
-                // efekt jest ten sam, ale warto wiedzieć na przyszlość.
-
-                repeat(pojedyńczeĆwiczenie.serie) { IndeksSerii ->
-
-                var powtórzeniaDanejSerii by remember { mutableStateOf("") }
-                var ciężarDanejSerii by remember { mutableStateOf("")}
-
-                    OutlinedTextField(
-                        value = ciężarDanejSerii,
-                        onValueChange = { ciężarDanejSerii = it },
-                        label = { Text("Ciężar") },
-                        modifier = Modifier.fillMaxWidth()
+            } else {
+                listaĆwiczeń.forEach { pojedyńczeĆwiczenie ->
+                    Text(
+                        "${pojedyńczeĆwiczenie.nazwa} " +
+                                "- ilość serii: ${pojedyńczeĆwiczenie.serie} ",
+                        color = MaterialTheme.colorScheme.onSurface
                     )
+                    Button(
+                        onClick = { listaĆwiczeń.remove(pojedyńczeĆwiczenie) }
+                    ) {
+                        Text("Usuń Ćwiczenie")
+                    }
 
-                    OutlinedTextField(
-                        value = powtórzeniaDanejSerii,
-                        onValueChange = { powtórzeniaDanejSerii = it },
-                        label = { Text("Ilość wykonanych powtórzeń") },
-                        modifier = Modifier.fillMaxWidth()
-                    )
 
-                    Spacer(modifier = Modifier.height(8.dp))
+                    // Powtórz tyle razy, ile jest serii -
+                    // tutaj repeat przekazuje iteracje ilosci serii (indeks) czyli
+                    // tak naprawdę 0, 1 i 2 (w przypadku 3 serii)
+                    // efekt jest ten sam, ale warto wiedzieć na przyszlość.
 
+                    repeat(pojedyńczeĆwiczenie.serie) { IndeksSerii ->
+
+                        var powtórzeniaDanejSerii by remember { mutableStateOf("") }
+                        var ciężarDanejSerii by remember { mutableStateOf("") }
+
+                        OutlinedTextField(
+                            value = ciężarDanejSerii,
+                            onValueChange = { ciężarDanejSerii = it },
+                            label = { Text("Ciężar") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+
+                        OutlinedTextField(
+                            value = powtórzeniaDanejSerii,
+                            onValueChange = { powtórzeniaDanejSerii = it },
+                            label = { Text("Ilość wykonanych powtórzeń") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                    }
                 }
             }
+
+            Spacer(Modifier.height(80.dp))
+
         }
+        PrzyciskPowrotuDoGłównegoEkranu(
+            navController,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.BottomEnd)
+        )
     }
-
-
-
-
-    PrzyciskPowrotuDoGłównegoEkranu(navController)
 }
